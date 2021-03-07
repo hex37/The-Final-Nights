ff947de82e8416a4a448dcf3715e941ff3c02a6a
Aleksej Komarov <stylemistake@gmail.com>
2021-03-07 02:06:17 +0200

    Fix tgui reloading (#57499)

--- tgui/packages/tgui-dev-server/reloader.js
+++ tgui/packages/tgui-dev-server/reloader.js
@@ -67,8 +67,8 @@ export const findCacheRoot = async () => {
 
 const onCacheRootFound = cacheRoot => {
   logger.log(`found cache at '${cacheRoot}'`);
-  // Plant dummy
-  // fs.closeSync(fs.openSync(cacheRoot + '/dummy', 'w'));
+  // Plant a dummy
+  fs.closeSync(fs.openSync(cacheRoot + '/dummy', 'w'));
 };
 
 export const reloadByondCache = async bundleDir => {
@@ -94,12 +94,12 @@ export const reloadByondCache = async bundleDir => {
     const garbage = await resolveGlob(cacheDir, './*.+(bundle|chunk|hot-update).*');
     try {
       for (let file of garbage) {
-        fs.unlink(file);
+        fs.unlinkSync(file);
       }
       // Copy assets
       for (let asset of assets) {
         const destination = resolvePath(cacheDir, basename(asset));
-        fs.copyFile(asset, destination);
+        fs.writeFileSync(destination, fs.readFileSync(asset));
       }
       logger.log(`copied ${assets.length} files to '${cacheDir}'`);
     }
