c4aa168611eb1b9442d5b76106d64ddccd2e05d1
Comrade Niobe <126028983+ComradeNiobe@users.noreply.github.com>
2025-03-13 15:44:11 -0400

    fix some stray css

--- tgui/packages/tgui/components/Box.js
+++ tgui/packages/tgui/components/Box.js
@@ -4,55 +4,55 @@
  * @license MIT
  */
 
-import { classes, pureComponentHooks } from 'common/react';
-import { createVNode } from 'inferno';
-import { ChildFlags, VNodeFlags } from 'inferno-vnode-flags';
-import { CSS_COLORS } from '../constants';
+import { classes, pureComponentHooks } from "common/react";
+import { createVNode } from "inferno";
+import { ChildFlags, VNodeFlags } from "inferno-vnode-flags";
+import { CSS_COLORS } from "../constants";
 
 /**
  * Coverts our rem-like spacing unit into a CSS unit.
  */
-export const unit = value => {
-  if (typeof value === 'string') {
+export const unit = (value) => {
+  if (typeof value === "string") {
     // Transparently convert pixels into rem units
-    if (value.endsWith('px') && !Byond.IS_LTE_IE8) {
-      return parseFloat(value) / 12 + 'rem';
+    if (value.endsWith("px") && !Byond.IS_LTE_IE8) {
+      return parseFloat(value) / 12 + "rem";
     }
     return value;
   }
-  if (typeof value === 'number') {
+  if (typeof value === "number") {
     if (Byond.IS_LTE_IE8) {
-      return value * 12 + 'px';
+      return value * 12 + "px";
     }
-    return value + 'rem';
+    return value + "rem";
   }
 };
 
 /**
  * Same as `unit`, but half the size for integers numbers.
  */
-export const halfUnit = value => {
-  if (typeof value === 'string') {
+export const halfUnit = (value) => {
+  if (typeof value === "string") {
     return unit(value);
   }
-  if (typeof value === 'number') {
+  if (typeof value === "number") {
     return unit(value * 0.5);
   }
 };
 
-const isColorCode = str => !isColorClass(str);
+const isColorCode = (str) => !isColorClass(str);
 
-const isColorClass = str => typeof str === 'string'
-  && CSS_COLORS.includes(str);
+const isColorClass = (str) =>
+  typeof str === "string" && CSS_COLORS.includes(str);
 
-const mapRawPropTo = attrName => (style, value) => {
-  if (typeof value === 'number' || typeof value === 'string') {
+const mapRawPropTo = (attrName) => (style, value) => {
+  if (typeof value === "number" || typeof value === "string") {
     style[attrName] = value;
   }
 };
 
 const mapUnitPropTo = (attrName, unit) => (style, value) => {
-  if (typeof value === 'number' || typeof value === 'string') {
+  if (typeof value === "number" || typeof value === "string") {
     style[attrName] = unit(value);
   }
 };
@@ -64,14 +64,14 @@ const mapBooleanPropTo = (attrName, attrValue) => (style, value) => {
 };
 
 const mapDirectionalUnitPropTo = (attrName, unit, dirs) => (style, value) => {
-  if (typeof value === 'number' || typeof value === 'string') {
+  if (typeof value === "number" || typeof value === "string") {
     for (let i = 0; i < dirs.length; i++) {
-      style[attrName + '-' + dirs[i]] = unit(value);
+      style[attrName + "-" + dirs[i]] = unit(value);
     }
   }
 };
 
-const mapColorPropTo = attrName => (style, value) => {
+const mapColorPropTo = (attrName) => (style, value) => {
   if (isColorCode(value)) {
     style[attrName] = value;
   }
@@ -79,92 +79,89 @@ const mapColorPropTo = attrName => (style, value) => {
 
 const styleMapperByPropName = {
   // Direct mapping
-  position: mapRawPropTo('position'),
-  overflow: mapRawPropTo('overflow'),
-  overflowX: mapRawPropTo('overflow-x'),
-  overflowY: mapRawPropTo('overflow-y'),
-  top: mapUnitPropTo('top', unit),
-  bottom: mapUnitPropTo('bottom', unit),
-  left: mapUnitPropTo('left', unit),
-  right: mapUnitPropTo('right', unit),
-  width: mapUnitPropTo('width', unit),
-  minWidth: mapUnitPropTo('min-width', unit),
-  maxWidth: mapUnitPropTo('max-width', unit),
-  height: mapUnitPropTo('height', unit),
-  minHeight: mapUnitPropTo('min-height', unit),
-  maxHeight: mapUnitPropTo('max-height', unit),
-  fontSize: mapUnitPropTo('font-size', unit),
-  fontFamily: mapRawPropTo('font-family'),
+  position: mapRawPropTo("position"),
+  overflow: mapRawPropTo("overflow"),
+  overflowX: mapRawPropTo("overflow-x"),
+  overflowY: mapRawPropTo("overflow-y"),
+  top: mapUnitPropTo("top", unit),
+  bottom: mapUnitPropTo("bottom", unit),
+  left: mapUnitPropTo("left", unit),
+  right: mapUnitPropTo("right", unit),
+  width: mapUnitPropTo("width", unit),
+  minWidth: mapUnitPropTo("min-width", unit),
+  maxWidth: mapUnitPropTo("max-width", unit),
+  height: mapUnitPropTo("height", unit),
+  minHeight: mapUnitPropTo("min-height", unit),
+  maxHeight: mapUnitPropTo("max-height", unit),
+  fontSize: mapUnitPropTo("font-size", unit),
+  fontFamily: mapRawPropTo("font-family"),
   lineHeight: (style, value) => {
-    if (typeof value === 'number') {
-      style['line-height'] = value;
-    }
-    else if (typeof value === 'string') {
-      style['line-height'] = unit(value);
+    if (typeof value === "number") {
+      style["line-height"] = value;
+    } else if (typeof value === "string") {
+      style["line-height"] = unit(value);
     }
   },
-  opacity: mapRawPropTo('opacity'),
-  textAlign: mapRawPropTo('text-align'),
-  verticalAlign: mapRawPropTo('vertical-align'),
+  opacity: mapRawPropTo("opacity"),
+  textAlign: mapRawPropTo("text-align"),
+  verticalAlign: mapRawPropTo("vertical-align"),
   // Boolean props
-  inline: mapBooleanPropTo('display', 'inline-block'),
-  bold: mapBooleanPropTo('font-weight', 'bold'),
-  italic: mapBooleanPropTo('font-style', 'italic'),
-  nowrap: mapBooleanPropTo('white-space', 'nowrap'),
+  inline: mapBooleanPropTo("display", "inline-block"),
+  bold: mapBooleanPropTo("font-weight", "bold"),
+  italic: mapBooleanPropTo("font-style", "italic"),
+  nowrap: mapBooleanPropTo("white-space", "nowrap"),
   // Margins
-  m: mapDirectionalUnitPropTo('margin', halfUnit, [
-    'top', 'bottom', 'left', 'right',
+  m: mapDirectionalUnitPropTo("margin", halfUnit, [
+    "top",
+    "bottom",
+    "left",
+    "right",
   ]),
-  mx: mapDirectionalUnitPropTo('margin', halfUnit, [
-    'left', 'right',
-  ]),
-  my: mapDirectionalUnitPropTo('margin', halfUnit, [
-    'top', 'bottom',
-  ]),
-  mt: mapUnitPropTo('margin-top', halfUnit),
-  mb: mapUnitPropTo('margin-bottom', halfUnit),
-  ml: mapUnitPropTo('margin-left', halfUnit),
-  mr: mapUnitPropTo('margin-right', halfUnit),
+  mx: mapDirectionalUnitPropTo("margin", halfUnit, ["left", "right"]),
+  my: mapDirectionalUnitPropTo("margin", halfUnit, ["top", "bottom"]),
+  mt: mapUnitPropTo("margin-top", halfUnit),
+  mb: mapUnitPropTo("margin-bottom", halfUnit),
+  ml: mapUnitPropTo("margin-left", halfUnit),
+  mr: mapUnitPropTo("margin-right", halfUnit),
   // Margins
-  p: mapDirectionalUnitPropTo('padding', halfUnit, [
-    'top', 'bottom', 'left', 'right',
+  p: mapDirectionalUnitPropTo("padding", halfUnit, [
+    "top",
+    "bottom",
+    "left",
+    "right",
   ]),
-  px: mapDirectionalUnitPropTo('padding', halfUnit, [
-    'left', 'right',
-  ]),
-  py: mapDirectionalUnitPropTo('padding', halfUnit, [
-    'top', 'bottom',
-  ]),
-  pt: mapUnitPropTo('padding-top', halfUnit),
-  pb: mapUnitPropTo('padding-bottom', halfUnit),
-  pl: mapUnitPropTo('padding-left', halfUnit),
-  pr: mapUnitPropTo('padding-right', halfUnit),
+  px: mapDirectionalUnitPropTo("padding", halfUnit, ["left", "right"]),
+  py: mapDirectionalUnitPropTo("padding", halfUnit, ["top", "bottom"]),
+  pt: mapUnitPropTo("padding-top", halfUnit),
+  pb: mapUnitPropTo("padding-bottom", halfUnit),
+  pl: mapUnitPropTo("padding-left", halfUnit),
+  pr: mapUnitPropTo("padding-right", halfUnit),
   // Color props
-  color: mapColorPropTo('color'),
-  textColor: mapColorPropTo('color'),
-  backgroundColor: mapColorPropTo('background-color'),
+  color: mapColorPropTo("color"),
+  textColor: mapColorPropTo("color"),
+  backgroundColor: mapColorPropTo("background-color"),
   // Utility props
   fillPositionedParent: (style, value) => {
     if (value) {
-      style['position'] = 'absolute';
-      style['top'] = 0;
-      style['bottom'] = 0;
-      style['left'] = 0;
-      style['right'] = 0;
+      style["position"] = "absolute";
+      style["top"] = 0;
+      style["bottom"] = 0;
+      style["left"] = 0;
+      style["right"] = 0;
     }
   },
 };
 
-export const computeBoxProps = props => {
+export const computeBoxProps = (props) => {
   const computedProps = {};
   const computedStyles = {};
   // Compute props
   for (let propName of Object.keys(props)) {
-    if (propName === 'style') {
+    if (propName === "style") {
       continue;
     }
     // IE8: onclick workaround
-    if (Byond.IS_LTE_IE8 && propName === 'onClick') {
+    if (Byond.IS_LTE_IE8 && propName === "onClick") {
       computedProps.onclick = props[propName];
       continue;
     }
@@ -172,21 +169,20 @@ export const computeBoxProps = props => {
     const mapPropToStyle = styleMapperByPropName[propName];
     if (mapPropToStyle) {
       mapPropToStyle(computedStyles, propValue);
-    }
-    else {
+    } else {
       computedProps[propName] = propValue;
     }
   }
   // Concatenate styles
-  let style = '';
+  let style = "";
   for (let attrName of Object.keys(computedStyles)) {
     const attrValue = computedStyles[attrName];
-    style += attrName + ':' + attrValue + ';';
+    style += attrName + ":" + attrValue + ";";
   }
   if (props.style) {
     for (let attrName of Object.keys(props.style)) {
       const attrValue = props.style[attrName];
-      style += attrName + ':' + attrValue + ';';
+      style += attrName + ":" + attrValue + ";";
     }
   }
   if (style.length > 0) {
@@ -195,29 +191,25 @@ export const computeBoxProps = props => {
   return computedProps;
 };
 
-export const computeBoxClassName = props => {
+export const computeBoxClassName = (props) => {
   const color = props.textColor || props.color;
   const backgroundColor = props.backgroundColor;
   return classes([
-    isColorClass(color) && 'color-' + color,
-    isColorClass(backgroundColor) && 'color-bg-' + backgroundColor,
+    isColorClass(color) && "color-" + color,
+    isColorClass(backgroundColor) && "color-bg-" + backgroundColor,
   ]);
 };
 
-export const Box = props => {
-  const {
-    as = 'div',
-    className,
-    children,
-    ...rest
-  } = props;
+export const Box = (props) => {
+  const { as = "div", className, children, ...rest } = props;
   // Render props
-  if (typeof children === 'function') {
+  if (typeof children === "function") {
     return children(computeBoxProps(props));
   }
-  const computedClassName = typeof className === 'string'
-    ? className + ' ' + computeBoxClassName(rest)
-    : computeBoxClassName(rest);
+  const computedClassName =
+    typeof className === "string"
+      ? className + " " + computeBoxClassName(rest)
+      : computeBoxClassName(rest);
   const computedProps = computeBoxProps(rest);
   // Render a wrapper element
   return createVNode(
@@ -226,7 +218,8 @@ export const Box = props => {
     computedClassName,
     children,
     ChildFlags.UnknownChildren,
-    computedProps);
+    computedProps
+  );
 };
 
 Box.defaultHooks = pureComponentHooks;
